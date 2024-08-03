fc23ee7a29d6292aee9b62ab463a343bbbe1b501
Santi <canosantiago404@gmail.com>
2024-08-03 18:19:10 -0300

    sidebar terminado

---
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.data.ts
@@ -0,0 +1,19 @@
+import { Calendar, Car, Heart } from "lucide-react";
+
+export const dataGeneralSidebar = [
+  {
+    icon: Car,
+    label: "Cars",
+    href: "/dashboard",
+  },
+  {
+    icon: Calendar,
+    label: "Cars Reserves",
+    href: "/reserves",
+  },
+  {
+    icon: Heart,
+    label: "Loved Cars",
+    href: "/loved-cars",
+  },
+];
--- app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.tsx
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,7 +1,32 @@
 "use client";
 
+import { useAuth } from "@clerk/nextjs";
+
 import { Separator } from "@/components/ui/separator";
+import { dataGeneralSidebar } from "./SidebarRoutes.data";
+import { SidebarItem } from "./SidebarItem";
 
 export function SidebarRoutes() {
-  return <div>SidebarRoutes</div>;
+  const { userId } = useAuth();
+
+  return (
+    <div className="flex flex-col justify-between h-full">
+      <div>
+        <div className="p-2 md:p-6">
+          <p className="mb-2 text-slate-500">GENERAL</p>
+          {dataGeneralSidebar.map((item) => (
+            <SidebarItem key={item.label} item={item} />
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <Separator />
+
+        <footer className="p-3 mt-3 text-center">
+          2024. All rights reserved
+        </footer>
+      </div>
+    </div>
+  );
 }

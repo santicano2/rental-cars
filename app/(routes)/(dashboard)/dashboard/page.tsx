413657fb9444eeb3b7dbcea48f1fc6a01cb28cae
Santi <canosantiago404@gmail.com>
2024-08-04 00:34:05 -0300

    añadido navbar

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,10 @@
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">List of cars</h2>
+      </div>
+      <p>Cars</p>
+    </div>
+  );
 }
--- app/(routes)/(dashboard)/layout.tsx
+++ app/(routes)/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { NavbarDashboard } from "./dashboard/components/NavbarDashboard";
 import { Sidebar } from "./dashboard/components/Sidebar";
 
 export default function LayoutDashboard({
@@ -11,7 +12,7 @@ export default function LayoutDashboard({
         <Sidebar />
       </div>
       <div className="w-full h-full xl:ml-80">
-        Navbar dashboard
+        <NavbarDashboard />
         <div className="p-6 h-max">{children}</div>
       </div>
     </div>

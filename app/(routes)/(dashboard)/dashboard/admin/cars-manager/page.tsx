583804c9260b00b8330fc7184c2b97d44be0db1c
Santi <canosantiago404@gmail.com>
2024-08-04 13:03:17 -0300

    añadida page de manage cars

---
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
@@ -0,0 +1,10 @@
+export default function CarsManagerPage() {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">Manage your cars</h2>
+        <p>Button...</p>
+      </div>
+    </div>
+  );
+}

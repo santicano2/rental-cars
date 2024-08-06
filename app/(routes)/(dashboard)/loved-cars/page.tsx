19d0aa17da608456c4478fe22bf08d28aeb5eec6
Santi <canosantiago404@gmail.com>
2024-08-06 13:59:02 -0300

    user validation on page

--- app/(routes)/(dashboard)/loved-cars/page.tsx
+++ app/(routes)/(dashboard)/loved-cars/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
+
 import { ListLovedCars } from "./components/ListLovedCars";
 
 export default function PageLovedCars() {
+  const { userId } = useAuth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   return (
     <div>
       <h1 className="text-2xl">Loved cars</h1>

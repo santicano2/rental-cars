932205401707e1b6a5cf23e7a724c45e8e3f3df3
Santi <canosantiago404@gmail.com>
2024-08-07 12:46:58 -0300

    añadido componente features

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/Shared/Navbar";
 
 import { FirstBlock } from "./components/FirstBlock";
 import { SliderBrands } from "./components/SliderBrands";
+import { Features } from "./components/Features";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <FirstBlock />
       <SliderBrands />
+      <Features />
     </div>
   );
 }

f41d6f4a45a1df6875de63577eda53dac3e27846
Santi <canosantiago404@gmail.com>
2024-08-07 11:34:12 -0300

    añadido framer motion con componente reveal

--- app/(routes)/(home)/components/FirstBlock/FirstBlock.tsx
+++ app/(routes)/(home)/components/FirstBlock/FirstBlock.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 
+import { Reveal } from "@/components/Shared/Reveal";
+
 export function FirstBlock() {
   return (
     <div className="grid lg:grid-cols-2 lg:px-0 lg:py-24 items-center">
-      <div className="p-6 lg:pl-40">
-        <h1 className="text-4xl lg:text-8xl font-bold">
-          Premium <span className="block">Car rental</span>
+      <Reveal className="p-6 lg:pl-40" position="bottom">
+        <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold">
+          Premium <span className="block">rental cars</span>
         </h1>
         <p className="text-lg mt-2 lg:mt-5 lg:text-xl max-w-sm">
           Drive the best premium cars from around the world with AutoExpress.
         </p>
-      </div>
+      </Reveal>
 
-      <div className="flex justify-end">
+      <Reveal className="flex justify-end" position="right">
         <Image
           src="/images/porsche.png"
           alt="Rent cars"
@@ -20,7 +22,7 @@ export function FirstBlock() {
           height={800}
           priority
         />
-      </div>
+      </Reveal>
     </div>
   );
 }

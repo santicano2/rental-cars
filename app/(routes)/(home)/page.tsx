286d3746c4e425e4e9f594649d36b647727aba2f
Santi <canosantiago404@gmail.com>
2024-08-07 13:50:52 -0300

    creado componente fleet

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "@/components/Shared/Navbar";
 import { FirstBlock } from "./components/FirstBlock";
 import { SliderBrands } from "./components/SliderBrands";
 import { Features } from "./components/Features";
+import { OurFleet } from "./components/OurFleet";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <FirstBlock />
       <SliderBrands />
       <Features />
+      <OurFleet />
     </div>
   );
 }
